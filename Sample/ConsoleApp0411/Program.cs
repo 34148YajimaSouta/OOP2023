40aaf676f2675cc4da30ca2e5148431209c82440
infosys <infosys@C4221>
2023-04-17 10:06:50 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample/ConsoleApp0411/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp0411 {
+    class Program {
+        static void Main(string[] args) {
+            int[] chengeMoney = { 10000, 5000, 2000,1000, 500, 100, 50, 10, 5, 1 };
+            Console.Write("金額:");
+            int sum = int.Parse(Console.ReadLine());
+            Console.Write("支払い:");
+            int money = int.Parse(Console.ReadLine());
+            int chenge = money-sum;
+            Console.WriteLine("お釣り:"+chenge);
+            for (int i = 0; i < chengeMoney.Length; i++)
+            {
+                Console.Write(chengeMoney[i] + "円:" );
+                for(int j = 0; j < chenge / chengeMoney[i]; j++)
+                {
+                    Console.Write("*");
+                }
+                chenge %= chengeMoney[i];
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
---
+++ Sample/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btButton = new System.Windows.Forms.Button();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.tbAns = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btButton
+            // 
+            this.btButton.Location = new System.Drawing.Point(44, 92);
+            this.btButton.Name = "btButton";
+            this.btButton.Size = new System.Drawing.Size(155, 83);
+            this.btButton.TabIndex = 0;
+            this.btButton.Text = "計算";
+            this.btButton.UseVisualStyleBackColor = true;
+            this.btButton.Click += new System.EventHandler(this.btButton_Click);
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Location = new System.Drawing.Point(70, 37);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(93, 19);
+            this.tbNum1.TabIndex = 1;
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Location = new System.Drawing.Point(239, 37);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(93, 19);
+            this.tbNum2.TabIndex = 1;
+            // 
+            // tbAns
+            // 
+            this.tbAns.Location = new System.Drawing.Point(434, 37);
+            this.tbAns.Name = "tbAns";
+            this.tbAns.Size = new System.Drawing.Size(93, 19);
+            this.tbAns.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(187, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "＋";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(379, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbAns);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.btButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btButton;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.TextBox tbAns;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
