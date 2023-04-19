458a19d90561baba89f23c0bfa658ef212976247
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-04-19 11:15:06 +0900

    乱数

--- Sample/BallApp/SoccerBall.cs
+++ Sample/BallApp/SoccerBall.cs
@@ -13,14 +13,19 @@ namespace BallApp {
         private double posX;    //ｘ座標
         private double posY;    //ｙ座標
 
-        private double moveX = 10.0;  //移動量（ｘ方向）
-        private double moveY = 10.0;  //移動量（ｙ方向）
+        private double moveX ;  //移動量（ｘ方向）
+        private double moveY ;  //移動量（ｙ方向）
 
         //コンストラクタ
-        public SoccerBall() {
+        public  SoccerBall(double xp,double yp) {
             Image = Image.FromFile(@"pic\soccer_ball.png");
-            PosX = 0.0;
-            PosY = 0.0;
+            Random r1 = new System.Random();
+            PosX = xp;
+            PosY = yp;
+            int rndX = r1.Next(-100, 100);
+            moveX = (rndX !=0 ? rndX:1);
+            int rndY = r1.Next(-100, 100);
+            moveY = (rndY != 0 ? rndY : 1);
         }
 
         //プロパティ
