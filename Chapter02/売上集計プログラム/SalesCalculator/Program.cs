404c9d9f257dbfddccfcc4c969e1d0ad3d5ce0b1
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-05-16 11:47:54 +0900

    var

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -9,29 +9,11 @@ namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
           
-            SalesCounter sales = new SalesCounter(ReadSales(@"data\Sales.csv"));
-            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string, int> obj in amountPerStore) {
-                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+            var sales = new SalesCounter(@"data\Sales.csv");
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0} {1:C}", obj.Key, obj.Value);
             }
-        }
-
-        //売上データを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {
-            List<Sale> sales = new List<Sale>();//売上データを格納
-            string[] lines = File.ReadAllLines(filePath);//ファイルからすべてのデータを読み込む
-
-            foreach (string line in lines) {//すべての行から1行ずつ取り出す
-                string[] items = line.Split(',');
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2])
-                };
-                sales.Add(sale);
-            }
-
-            return sales;
         }
     }
 }
