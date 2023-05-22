7ea6c1a75b503a89ea92aa251ec0e108fb561fa3
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-05-22 10:32:56 +0900

    コメント修正

--- Chapter02/演習問題/Exercise03/SalesCounter.cs
+++ Chapter02/演習問題/Exercise03/SalesCounter.cs
@@ -15,14 +15,14 @@ namespace Exercise03 {
             _sales = ReadSales(filePath);
         }
 
-        //カテゴリー別売上高を求める
+        //商品カテゴリ別売上高を求める
         public IDictionary<string, int> GetPerCategorySales() {
             var dict = new SortedDictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ProductCategory)) 
-                    dict[sale.ProductCategory] +=sale.Amount;//店名が既に存在する（売上加算）
+                    dict[sale.ProductCategory] +=sale.Amount;//商品カテゴリが既に存在する（売上加算）
                 else 
-                    dict[sale.ProductCategory] =sale.Amount;//店名が存在しない(新規格納)
+                    dict[sale.ProductCategory] =sale.Amount;//商品カテゴリが存在しない(新規格納)
                 
             }
             return dict;
