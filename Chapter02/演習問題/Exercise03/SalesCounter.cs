0b5a44b376d943f4d5c37f9835253eac00606181
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-05-22 10:01:09 +0900

    Exercise03

--- Chapter02/演習問題/Exercise03/SalesCounter.cs
+++ Chapter02/演習問題/Exercise03/SalesCounter.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SalesCalculator {
+namespace Exercise03 {
     public class SalesCounter {
 
         private IEnumerable<Sale> _sales;
