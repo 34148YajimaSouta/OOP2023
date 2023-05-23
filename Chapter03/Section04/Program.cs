e5025614679b3f68278826211aad5f552316d496
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-05-23 11:54:00 +0900

    第3章の演習問題1

---
+++ Chapter03/Section04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            //IEnumerable<string> query = names.Where(s => s.Length <= 5).Select(s=>s.ToLower());
+            var query = names.Select(s => s.Length);
+            foreach(var n in query) {
+                Console.WriteLine("{0}",n);
+            }
+        }
+    }
+}
