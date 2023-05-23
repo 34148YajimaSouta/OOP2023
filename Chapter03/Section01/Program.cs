5356564a4ae2b67556402bc7bf0540f95fa295ee
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-05-23 10:29:46 +0900

    P87

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -9,10 +9,15 @@ namespace Section01 {
 
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-           
-            int count = numbers.Count( n => n % 2 == 0);
-            Console.WriteLine(count);
+            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, 3, 1, 6, 4, 10, 35, 2, 7, 5 };
+
+
+            //5の倍数をカウント
+            //int count = numbers.Count( n => n%5==0 && n>0);
+
+            //合計値
+            var sum = numbers.Where(n=>n%2==0).Sum();
+            Console.WriteLine(sum);
 
         }
 
