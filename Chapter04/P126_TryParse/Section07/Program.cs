ff57d72a82eecd221696e148b22c6223a0a6b740
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-05-30 10:10:30 +0900

    127pまで

---
+++ Chapter04/P126_TryParse/Section07/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section07 {
+    class Program {
+        static void Main(string[] args) {
+            var str = "123";
+            int height;
+            if (int.TryParse(str, out height)) {
+                //変換に成功した時の処理
+            }
+            else {
+                //変換に失敗した時の処理
+            }
+        }
+    }
+}
