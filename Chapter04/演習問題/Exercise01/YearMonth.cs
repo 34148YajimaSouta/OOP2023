7de009bd02e5e45a720178245d00f86e40b0ab6e
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-05-30 10:26:07 +0900

    第4章演習問題4.1

---
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class YearMonth {
+        public int Year { get; private set; }
+
+        public int Month { get; private set; }
+
+
+        public YearMonth(int Year,int Month) {
+            this.Year = Year;
+            this.Month = Month;
+        }
+    }
+    
+}
