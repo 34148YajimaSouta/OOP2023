8d7ac09a025c52ff2cacdff806721d5dd1d63cc8
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-05-31 09:52:44 +0900

    演習問題4.2.2

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -8,13 +8,53 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            #region
+            //var ym = new YearMonth(2023, 5);
+            //var c21 = ym.Is21Century;
+            //var ymNextMonth = ym.AddOneMonth();
 
-            var ym = new YearMonth(2023, 5);
-            var c21 = ym.Is21Century;
-            var ymNextMonth = ym.AddOneMonth();
+            //Console.WriteLine(ym);
+            //Console.WriteLine(ymNextMonth);
+            #endregion
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
+            };
 
-            Console.WriteLine(ym);
-            Console.WriteLine(ymNextMonth);
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var item in ymCollection) {
+                Console.WriteLine(item);
+            }
+        }
+
+        //4.2.3
+        //static YearMonth FindFirst21C(YearMonth[] yms) {
+
+        //}
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+
+        }
+        private static void Exercise2_5(YearMonth[] ymCollection) {
 
         }
     }
