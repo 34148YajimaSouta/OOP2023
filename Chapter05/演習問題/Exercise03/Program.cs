3b0fdb28d6563949c0ce9e3fe40556a5df650b71
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-06-05 12:03:38 +0900

    演習問題5.3.5まで

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -34,19 +34,27 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_3(string text) {
-            string[] words = text.Split(' ');
-
-            foreach (var word in words) {
-                System.Console.WriteLine($"{word}");
-            }
+            int count = text.Split(' ').Length;
+            Console.WriteLine("単語数:{0}", count);
         }
 
         private static void Exercise3_4(string text) {
-            
+            var words = text.Split(' ').Where(s => s.Length >= 4);
+            foreach (var word in words)
+                Console.WriteLine(word);
         }
 
         private static void Exercise3_5(string text) {
-            
+            var array = text.Split(' ').ToArray();
+          
+            var sb = new StringBuilder(array[0]);
+            foreach(var word in array.Skip(1)) {
+                sb.Append(' ');
+                sb.Append(word);
+                
+            }
+            var createWords = sb.ToString();
+            Console.WriteLine(createWords);
         }
     }
 }
