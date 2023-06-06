c5fec3baf2c9326bf11466ae11cc005174500341
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-06-06 09:44:10 +0900

    演習問題5.4完成

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -8,6 +8,11 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var replace = line.Replace("=", ":").Replace("Novelist", "作家　").Replace("BestWork", "代表作").Replace("Born","誕生年");
+            var array = replace.Split(';').ToArray();
+            foreach (var item in array) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
