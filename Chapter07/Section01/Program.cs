ffd5bedc5d824ef7596cd221995ccb35e75452ac
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-06-22 11:21:43 +0900

    重複を考慮しない県庁所在地の登録

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        static void Main(string[] args) {
+            var kentyou = new Dictionary<string, string>() {
+                
+            };
+            Console.WriteLine("県庁所在地の登録");
+            Console.Write("県名：");
+            var kenmei =Console.ReadLine();
+            while (kenmei != "999") {
+                Console.Write("所在地：");
+                var shozaiti = Console.ReadLine();
+                kentyou[kenmei] = shozaiti;
+                Console.Write("県名：");
+                kenmei = Console.ReadLine();
+            };
+
+            Console.Write("県名を入力:");
+            kenmei = Console.ReadLine();
+            Console.WriteLine(kentyou[kenmei]+"です");
+
+            
+        }
+    }
+}
