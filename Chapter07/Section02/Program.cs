48e82fc1103a9600a5d3cc837e1acd1d3dae5823
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-06-29 11:39:16 +0900

    演習問題7.1

---
+++ Chapter07/Section02/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section02 {
+    class Program {
+        static void Main(string[] args) {
+
+            var prefDict = new Dictionary<string, List<CityInfo>>();
+            string pref, city;
+            int population;
+
+            Console.WriteLine("都市の登録");
+            while (true) {
+                Console.Write("県名：");
+                pref = Console.ReadLine();
+                if (pref == "999") break;
+
+                Console.Write("市町村：");
+                city = Console.ReadLine();
+
+                Console.Write("人口：");
+                population = int.Parse(Console.ReadLine());
+
+                //市町村情報インスタンスの生成
+                var cityInfo = new CityInfo {
+                    City = city,
+                    Population = population,
+                };
+
+                if (!prefDict.ContainsKey(pref)) {
+                    prefDict[pref] = new List<CityInfo>(); //既に県名が未登録ならリスト作成
+                }
+                prefDict[pref].Add(cityInfo);
+            }
+            Console.WriteLine();
+            Console.WriteLine("1:一覧表示,2:県名指定");
+            Console.Write(">");
+            var selected = Console.ReadLine();
+
+            if (selected == "1") {
+                //一覧表示
+                foreach (var prefData in prefDict) {
+                    foreach (var cityData in prefData.Value) {
+                        Console.WriteLine("{0}【{1}(人口：{2}人)】", prefData.Key, cityData.City, cityData.Population);
+                    }
+                }
+            }
+            else {
+                //県名指定表示
+                Console.Write("県名を入力：");
+                var inputPref = Console.ReadLine();
+                foreach (var cityData in prefDict[inputPref]) {
+                    Console.WriteLine("{0}【{1}(人口：{2}人)】", inputPref, cityData.City, cityData.Population);
+                }
+            }
+        }
+    }
+}
+
+class CityInfo {
+    public string City { get; set; }        //都市
+    public int Population { get; set; }     //人口
+}
+
---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+            Exercise1_1(text);
+            Console.WriteLine();
+            Exercise1_2(text);
+        }
+
+        private static void Exercise1_1(string text) {
+            var dict = new Dictionary<Char, int>();
+            var replaced =text.ToUpper();
+            Console.WriteLine(replaced);
+            foreach (var ch in replaced) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch] += 1;
+                    }
+                    else {
+                        dict[ch] = 1;
+                    }
+                }
+                
+            }
+            foreach (var item in dict.OrderBy(c=>c.Key)) {
+                Console.WriteLine("'{0}':{1}", item.Key, item.Value);
+            }
+        }
+
+        private static void Exercise1_2(string text) {
+            var dict = new SortedDictionary<Char, int>();
+            var replaced = text.ToUpper();
+            Console.WriteLine(replaced);
+            foreach (var ch in replaced) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch)) {
+                        dict[ch] += 1;
+                    }
+                    else {
+                        dict[ch] = 1;
+                    }
+                }
+
+            }
+            foreach (var item in dict) {
+                Console.WriteLine("'{0}':{1}", item.Key, item.Value);
+            }
+        }
+    }
+}
