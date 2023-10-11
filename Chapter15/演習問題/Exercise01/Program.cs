0afd9e1638a0ebaec2d5c9b00dd768eee3bcf7e7
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-10-11 12:01:41 +0900

    演習問題15-1-2

---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,53 @@
+﻿using Chapter15;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var price = Library.Books.First(b => b.Price ==Library.Books.Max(x => x.Price));
+            Console.WriteLine(price);
+        }
+
+        private static void Exercise1_3() {
+            var books = Library.Books.GroupBy(b => b.PublishedYear);
+            Console.WriteLine(books);
+        }
+
+        private static void Exercise1_4() {
+        }
+
+        private static void Exercise1_5() {
+        }
+
+        private static void Exercise1_6() {
+        }
+
+        private static void Exercise1_7() {
+        }
+
+        private static void Exercise1_8() {
+        }
+    }
+}
