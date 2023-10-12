d9e0c574de5ecb455b9683472c54007394011373
矢島　颯大 <OIBC34148@ojs.ac.jp>
2023-10-12 12:08:45 +0900

    演習問題15-1-6

--- Chapter15/演習問題/Exercise01/Program.cs
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -38,12 +38,56 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_4() {
+            var query = Library.Books.Join(Library.Categories,
+                                                   book => book.CategoryId,
+                                                   category => category.Id,
+                                                   (book, category) => new {
+                                                       book.PublishedYear,
+                                                       book.Price,
+                                                       book.Title,
+                                                       CategoryName = category.Name,
+                                                   }
+                                                   ).OrderBy(x => x.PublishedYear).ThenByDescending(x => x.Price);
+            foreach (var item in query) {
+                Console.WriteLine("{0}年{1}円{2} ({3})",
+                                  item.PublishedYear,
+                                  item.Price,
+                                  item.Title,
+                                  item.CategoryName
+                    );
+            }
+
+                ;
         }
 
         private static void Exercise1_5() {
+            var books = Library.Books.Where(b=>b.PublishedYear==2016).Join(Library.Categories,
+                                                   book => book.CategoryId,
+                                                   category => category.Id,
+                                                   (book, category) =>category.Name).Distinct();
+            foreach (var item in books) {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise1_6() {
+            var query = Library.Books.Join(Library.Categories,
+                                                   book => book.CategoryId,
+                                                   category => category.Id,
+                                                   (book, category) => new {
+                                                       book.PublishedYear,
+                                                       book.Price,
+                                                       book.Title,
+                                                       CategoryName = category.Name,
+                                                   })
+                                                   .GroupBy(x => x.CategoryName)
+                                                   .OrderBy(x => x.Key);
+            foreach (var group in query) {
+                Console.WriteLine("#{0}", group.Key);
+                foreach (var item in group) {
+                    Console.WriteLine(" {0}", item.Title);
+                }
+            }
         }
 
         private static void Exercise1_7() {
